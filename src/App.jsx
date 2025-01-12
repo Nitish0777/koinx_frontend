3c4d0db253286d3156a1af1bdc3946a4face0844
Nitish Nashine <nitishnashine@gmail.com>
2025-01-12 11:59:25 +0530

    add Bitcoin chart component and integrate it into App; update package.json to include recharts dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-import './App.css'
-import Navbar from './components/Navbar/Navbar'
+import "./App.css";
+import BitcoinChart from "./components/charts/Bitcoin";
+import BitcoinPerformance from "./components/charts/BitcoinPer";
+// import BitcoinTracker from "./components/charts/Charts";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-
   return (
     <>
       <Navbar />
+      {/* <BitcoinTracker /> */}
+      <BitcoinChart />
+      <BitcoinPerformance />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
